e31e43e4f92f93de3ad7dd0c2bfa19b51f2a6b9b
Andrew Grote <asgrote1@gmail.com>
2019-02-24 22:30:19 -0600

    complete first draft of assignment 2.

--- data-visualization/programming-assignment-2/scripts/assignment.R
+++ data-visualization/programming-assignment-2/scripts/assignment.R
@@ -1,25 +1,191 @@
-# ASSIGNMENT PROMPT:  In addition to your visualization, please include a
-# paragraph that helps explain your submission. A few questions that your
-# paragraph could answer include:
+# Submission
+# You must upload an image of your visualization for peer evaluation.
+# In addition to your visualization, please include a paragraph that helps explain your submission. A few questions that your paragraph could answer include:
+#     1. What is the data set that you chose? Why?
 #
-#     1.    What are your x- and y-axes?
-#     2.    Did you use a subset of the data? If so, what was it?
-#     3.    Are there any particular aspects of your visualization to which you
+#     2. Did you use a subset of the data? If so, what was it?
+#
+#     3. Are there any particular aspects of your visualization to which you would like to bring attention?
+#
+#     4. What do you think the data and your visualization show?
+#
+#   I've learned the hard way that R is not the tool to use for constructing
+#   many of the standard network graphs. However, it functions well to create maps.
+#
+#       1.  What is the data that you chose? Why?
+#
+#           The data can be found here: http://kateto.net/network-visualization.
+#           This chart is modeled heavily off of Katya Ognyanova's final example
+#           at the aforementioned link, but it goes several steps further and
+#           focuses on airports with few connections rather than many. The data
+#           were chosen after hitting a dead end with patent citation data and a
+#           handful of other data sets.
+#
+#       2.  Did you use a subset of the data? If so, what was it?
+#
+#           Yes. I restricted the data to airports that fly to 5 or fewer
+#           airports.
+#
+#       3.  Are there any particular aspects of your visualization to which you
 #           would like to bring attention?
-#     4.    What do you think the data and your visualization show?
 #
-# I took advantage of the provided data and aggregated the global temperature
-# deviations by decade, calculating the average temperature deviation, maximum
-# deviation, and minimum deviation for each. The average was used to construct a
-# bar chart, with the max and min used to overlay error bars. My x-axis
-# represents decades as a nominal variable, while the y-axis is temperature
-# deviation in 0.01 degrees (Celsius?).
+#           Contrast makes it easy to discern the different chart elements.
+#           Moreover, I took advantage of size and color to differentiate
+#           between airports with many (5) or few (1) connections via outbound
+#           flights.
 #
-# I took care to maintain a reasonably minimal design implemented via ggplot2 in
-# R; low saturation fill colors with full saturation borders and error bars,
-# no tick marks along the axes, no x-axis label as context renders it unnecessary,
-# etc. The chart makes clear that the trend is rising temperature, even when
-# accounting for the dispersion of deviations throughout each decade.
+#       4.  What do you think the data and your visualization show?
+#
+#           Notice that there are a surprising number of long-distance flights
+#           to and from small airports. This was a surprise (assuming the
+#           underlying data are correct).
+
+
+# install.packages("igraph")
+# install.packages("network")
+# install.packages("sna")
+# install.packages("visNetwork")
+# install.packages("threejs")
+# install.packages("networkD3")
+# install.packages("ndtv")
+# install.packages("maps")
+# install.packages("geosphere")
+# install.packages("Cairo")
 
 library("packrat")
-packrat::init()
+library("igraph")
+library("network")
+library("sna")
+library("visNetwork")
+library("threejs")
+library("networkD3")
+library("ndtv")
+library("maps")
+library("geosphere")
+library("Cairo")
+
+
+
+png(filename = "figures/assignment2.png",
+    type = c("cairo-png"))
+
+# CairoPNG(filename = "figures/assignment2.png")
+# CairoJPEG(filename = "figures/assignment2.jpg")
+
+# Load airport data
+
+airports <- read.csv(
+    file = "data/raw/Dataset3-Airlines-NODES.csv",
+    header = TRUE,
+    sep = ","
+)
+
+head(airports)
+
+
+flights <- read.csv(
+    file = "data/raw/Dataset3-Airlines-EDGES.csv",
+    header = TRUE,
+    sep = ","
+)
+
+head(flights)
+
+
+# Append the number of connections to the airports directory.
+
+tab <- table(flights$Source)
+
+airports <- airports[airports$ID %in% unique(flights$Source), ]
+
+airports$Connections <- tab
+
+
+# Keep only airports with flights out to fewer than 5 other aiports.
+
+idx <- names(tab)[tab <= 5]
+
+flights <- flights[flights$Source %in% idx & flights$Target %in% idx, ]
+
+airports <- airports[airports$ID %in% unique(c(flights$Source, flights$Target)), ]
+
+
+# Create color scheme for aiport colors:
+#   *   grey: is few connections
+#   *   orange: is many
+col_beg <- "grey70"
+#col_beg <- "orange_red"
+
+col_end <- "orange"
+
+col_fn <- colorRampPalette(c(col_beg, col_end))
+
+airports$Color <- col_fn(10)[as.numeric(cut(airports$Connections, breaks = 10))]
+
+
+# Plot a map of the united states dark color, appealing contrast and
+# overlay the airports. The size of the dot depends on the number of outbound
+# connections: 1-5.
+
+map("state", col = "grey20", fill = TRUE, bg = "black", lwd = 0.1)
+
+points(x = airports$longitude,
+       y = airports$latitude,
+       pch = 19,
+       cex = as.double(airports$Connections) / max(airports$Connections) * 1.25,
+       col = airports$Color)
+
+
+# Plot the flight paths. The color scheme is similar to that used for the
+# airports, with low-frequency flights being grey and high-frequency flights
+# being orange. The difference is that the flight paths are transparent.
+
+col_beg_tran <- adjustcolor(col_beg, alpha=0.4)
+
+col_end_tran <- adjustcolor(col_end, alpha=0.4)
+
+edge_col_fn <- colorRampPalette(c(col_beg_tran, col_end_tran), alpha = TRUE)
+
+edge_col <- edge_col_fn(100)
+
+for(i in 1:nrow(flights)) {
+
+    node1 <- airports[airports$ID == flights[i, ]$Source, ]
+
+    node2 <- airports[airports$ID == flights[i, ]$Target, ]
+
+    arc <- gcIntermediate(p1 = c(node1[1, ]$longitude, node1[1, ]$latitude),
+                          p2 = c(node2[1, ]$longitude, node2[1, ]$latitude),
+                          n = 1000,
+                          addStartEnd = TRUE )
+
+    edge_intensity <- round(100 * flights[i, ]$Freq / max(flights$Freq))
+
+    lines(arc, col = edge_col[edge_intensity], lwd = edge_intensity / 30)
+
+}
+
+
+# Add a legend and title to the plot.
+
+idx <- order(unique(airports$Connections))
+
+legend("bottomleft",
+       legend = unique(airports$Connections)[idx],
+       title = "Destinations (#)",
+       bg = "black",
+       text.col = "white",
+       pt.bg = unique(airports$Color)[idx],
+       pt.cex = seq(1, 5) / 5.0 * 1.25,
+       pch = 21,
+       cex = .8,
+       bty = "n",
+       ncol = 5)
+
+title(main ="Airports Flying to 5 or Fewer Destinations",
+      col.main = "white")
+
+
+# Save the plot!
+
+dev.off()
